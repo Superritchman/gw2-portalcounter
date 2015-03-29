495c146da5b782a0d96f7336cbd3dc731dc53fb3
Superritchman <Superritchman@gmail.com>
2015-03-29 18:01:57 +0200

    Add basic functions

---
+++ MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PortalCounter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_Time = new System.Windows.Forms.Label();
+            this.ti_CountDown = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl_Time
+            // 
+            this.lbl_Time.AutoSize = true;
+            this.lbl_Time.Font = new System.Drawing.Font("LetterOMatic!", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Time.ForeColor = System.Drawing.Color.White;
+            this.lbl_Time.Location = new System.Drawing.Point(1, 0);
+            this.lbl_Time.Name = "lbl_Time";
+            this.lbl_Time.Size = new System.Drawing.Size(75, 44);
+            this.lbl_Time.TabIndex = 0;
+            this.lbl_Time.Text = "60";
+            this.lbl_Time.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseClick);
+            this.lbl_Time.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDoubleClick);
+            this.lbl_Time.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            // 
+            // ti_CountDown
+            // 
+            this.ti_CountDown.Interval = 1000;
+            this.ti_CountDown.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(72, 47);
+            this.Controls.Add(this.lbl_Time);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Portal Counter";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.Activated += new System.EventHandler(this.MainForm_Activated);
+            this.Deactivate += new System.EventHandler(this.MainForm_Deactivate);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseClick);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDoubleClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Time;
+        private System.Windows.Forms.Timer ti_CountDown;
+    }
+}
+
---
+++ Settings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PortalCounter
+{
+    public partial class Settings : Form
+    {
+        private char newKey;
+
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void Settings_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            newKey = e.KeyChar;
+            this.lbl_DescNew.Text = "New Key: " + Char.ToUpper(newKey);
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            PortalCounter.Properties.Settings.Default.HotKey = newKey;
+            this.Close();
+        }
+
+        private void Settings_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Program.hook = true;
+        }
+
+        private void Settings_Shown(object sender, EventArgs e)
+        {
+            Program.hook = false;
+        }
+    }
+}
